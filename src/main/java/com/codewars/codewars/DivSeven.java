06d11f823399ba05132b28d8c00f568b9c7d26f0
Dawid <dawid.badurakrk@gmail.com>
2021-01-27 20:28:06 +0100

    first task

---
+++ src/main/java/com/codewars/codewars/DivSeven.java
@@ -0,0 +1,15 @@
+package com.codewars.codewars;
+
+class DivSeven {
+    public static long[] seven(long m) {
+        int i = 0;
+        while(m>99){
+            m = m/10-m%10*2;
+            i++;
+        }
+        long longArr[] = new long[2];
+        longArr[0]=m;
+        longArr[1]=i;
+        return longArr;
+    }
+}
