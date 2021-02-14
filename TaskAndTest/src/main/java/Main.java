b84bca28a4e12cf11d2d696c1c24e8aaf1933c03
Dawid <dawid.badurakrk@gmail.com>
2021-02-14 23:14:17 +0100

    quiz 5.2 java 8 in action

--- TaskAndTest/src/main/java/Main.java
+++ TaskAndTest/src/main/java/Main.java
@@ -1,6 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
+
 public class Main {
+
+
+
     public static void main(String[] args){
-        System.out.println("hej hej");
+//streams and lambda expresions
+        //quiz 5.2
+        //1
+        List<Integer>numbers =  Arrays.asList(1,2,3,4,5);
+        List<Integer>squares = numbers.stream()
+                .map(n -> n*n)
+                .collect(toList());
+        //2
+        List<Integer>numbers1 = Arrays.asList(1,2,3);
+        List<Integer>numbers2 = Arrays.asList(3,4);
+        List<int[]>pairs = numbers1.stream()
+                .flatMap(i -> numbers2.stream()
+                .map(j -> new int[]{i, j}))
+                .collect(toList());
+        //3
+        List<int[]>pairsDivBy3 = numbers1.stream()
+                .flatMap(i -> numbers2.stream()
+                        .filter(j -> (j+i)%3==0)
+                        .map(j -> new int[]{i, j}))
+                .collect(toList());
+        pairsDivBy3.stream().forEach(ints -> System.out.println(ints[0]+" "+ints[1]));
+
 
     }
 }
